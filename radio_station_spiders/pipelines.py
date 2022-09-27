8b9d76ec8a4baded56865caf18d9b26398678ecf
liuyanan <>
2022-09-27 12:33:23 +0800

    backup

--- radio_station_spiders/pipelines.py
+++ radio_station_spiders/pipelines.py
@@ -10,8 +10,13 @@ import logging
 import sqlite3
 
 conn = sqlite3.connect('test.db')
-
+#VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')''', (item['country_name'], item['country_iso_code'], item['city_name'], item['freq'], item['icon'], item['name'], item['transmitter'], item['stream_url']))
 class RadioStationSpidersPipeline:
     def process_item(self, item, spider):
+        c = conn.cursor()
+        c.execute('''INSERT INTO  "radio_stations" \
+("country_name", "country_iso_code", "city_name", "freq", "icon", "name", "transmitter", "stream_url") \
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', (item['country_name'], item['country_iso_code'], item['city_name'], item['freq'], item['icon'], item['name'], item['transmitter'], item['stream_url']))
+        conn.commit()        
         logging.log(logging.INFO, item)
         return item
--- radio_station_spiders/spiders/worldradiomap.py
+++ radio_station_spiders/spiders/worldradiomap.py
@@ -4,6 +4,7 @@ import urllib
 from copy import deepcopy
 from radio_station_spiders.items import RadioStationSpidersItem
 import time
+import pickle
 
 
 '''
@@ -36,8 +37,7 @@ class WorldRadiomapSpider(scrapy.Spider):
             country_name = country_page.xpath("@title").get().split('in ')[1].strip('the ')
             href = country_page.xpath("@href").get()
             station_info = {'country_name' : country_name}
-            meta = deepcopy({'station_info': station_info})
-            yield scrapy.Request(href, self.parse_city, meta = meta)
+            yield scrapy.Request(href, self.parse_city, meta = {'station_info': pickle.dumps(station_info)})
 
     def parse_city(self, response):
         # from https://worldradiomap.com/us/
@@ -45,8 +45,9 @@ class WorldRadiomapSpider(scrapy.Spider):
         city_pages = response.xpath('//h1[@class="title"]/a[@href]/@href').getall()
         for page in city_pages:
             page_abs_url = urllib.parse.urljoin(response.url, page)
-            response.meta["station_info"].update(deepcopy({'country_iso_code': country_iso_code}))
-            yield scrapy.Request(page_abs_url, self.parse_stations, meta=response.meta)
+            station_info = pickle.loads(response.meta["station_info"])
+            station_info.update({'country_iso_code': country_iso_code})
+            yield scrapy.Request(page_abs_url, self.parse_stations, meta={'station_info': pickle.dumps(station_info)})
             
     def parse_stations(self, response):
         # from https://worldradiomap.com/ca/winnipeg
@@ -60,8 +61,9 @@ class WorldRadiomapSpider(scrapy.Spider):
             station_detail_page = urllib.parse.urljoin(response.url, station.xpath('td[contains(@class,"fsta")]/a/@href').get())
             station_icon = urllib.parse.urljoin(response.url, station.xpath('td[contains(@class,"fsta")]/a/img/@src').get())
             station_name = "".join( station.xpath('td[contains(@class,"fsta")]/a/text()').getall()).strip(' \n\t')
-            station_transmitter = station.xpath('td[contains(@class,"fpre")]/text()').get()
-            station_info = {
+            station_transmitter = station.xpath('td[contains(@class,"fpre")]/text()').get().strip(', ')
+            station_info = pickle.loads(response.meta["station_info"])
+            station_info.update({
                 "src_url": response.url,
                 "locations": locations,
                 "city_name": city_name,
@@ -69,29 +71,25 @@ class WorldRadiomapSpider(scrapy.Spider):
                 "icon": station_icon,
                 "name": station_name,
                 "transmitter": station_transmitter,
-                }
-            if station_transmitter is None:
-                print(station.xpath('@class').get(), station_info)
-                exit(0)
-            response.meta["station_info"].update(deepcopy(station_info))
-            yield scrapy.Request(station_detail_page, self.parse_station_stream_url, meta=response.meta)
+                })
+            if station_transmitter is not None:
+                yield scrapy.Request(station_detail_page, self.parse_station_stream_url, meta={'station_info': pickle.dumps(station_info)})
         
     def parse_station_stream_url(self, response):
         time.sleep(0.1)
-        # print(response.meta)
-        response.meta['station_info']['stream_url'] = ''
+        station_info = pickle.loads(response.meta["station_info"])
         stream_url = response.xpath("//audio[@id='radiomapplayer']/@src").get()
         if stream_url:
             item = RadioStationSpidersItem()
-            item['country_name'] = response.meta['station_info']['country_name']
-            item['country_iso_code'] = response.meta['station_info']['country_iso_code']
-            item['city_name'] = response.meta['station_info']['city_name']
-            item['freq'] = response.meta['station_info']['freq']
-            item['icon'] = response.meta['station_info']['icon']
-            item['name'] = response.meta['station_info']['name']
-            item['transmitter'] = response.meta['station_info']['transmitter']
+            item['country_name'] = station_info['country_name']
+            item['country_iso_code'] = station_info['country_iso_code']
+            item['city_name'] = station_info['city_name']
+            item['freq'] = station_info['freq']
+            item['icon'] = station_info['icon']
+            item['name'] = station_info['name']
+            item['transmitter'] = station_info['transmitter']
             item['stream_url'] = stream_url 
-            item['src_url'] = response.meta['station_info']['src_url']
+            item['src_url'] = station_info['src_url']
             yield item
         
         
